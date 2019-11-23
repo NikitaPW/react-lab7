ff50ed6cdce386b15b60e2909216228b9a1a1703
NikitaPW <n-zaxarov99@mail.ru>
2019-11-23 15:18:56 +0100

    Lab7_Task3

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
+function isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
+
+function isPhoneNumber(number) {
+    var num = /^([0-9]{9})+$/;
+    return num.test(number);
+}
+
 class Form extends React.Component{
     constructor(props)
     {
@@ -36,14 +46,17 @@ class Form extends React.Component{
                 <br/><br/>
                 <label>Email: </label>
                 <input name="email" onChange={this.onChange}></input>
+                    {(isEmail(this.state.email) || (this.state.email == "")) ? "" : <label style={{color:'red', marginLeft:'10px'}}>The Email is not valid!</label>}
                 </div>
                  :
                 <div>
                 <label>Parent Name: </label>
                 <input name="name" onChange={this.onChange}></input>
-                <br/><br/>
+                    <br/><br/>
                 <label>Parent Phone Number: </label>
                 <input name="phone" onChange={this.onChange}></input>
+                    {(isPhoneNumber(this.state.phone) || (this.state.phone == "")) ? "" :
+                    <label style={{color:'red', marginLeft:'10px'}}>The phone number is not valid! Provide 9 digits of your phone number.</label> }
                 </div>}
             </div>}
             </div>
